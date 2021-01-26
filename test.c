ab5eddd26f5eb5879cfb339c5c8aad421d981ba6
hucaixin <77830983+hucaixin@users.noreply.github.com>
2021-01-26 21:30:24 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,143 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//猜数字游戏
+//#include<stdlib.h>
+//#include<time.h>
+//void menu()
+//{
+//	printf("*****************************\n");
+//	printf("*********** 1.play **********\n");
+//	printf("*********** 0.exit **********\n");
+//	printf("*****************************\n");
+//}
+//
+//
+//void game()
+//{
+//	int random_num = rand() % 100 + 1;
+//	int input = 0;
+//	while (1)
+//	{
+//		printf("请输入猜的数字>:");
+//		scanf("%d", &input);
+//		if (input > random_num)
+//		{
+//			printf("猜大了\n");
+//		}
+//		else if (input < random_num)
+//		{
+//			printf("猜小了\n");
+//		}
+//		else
+//		{
+//			printf("恭喜你，猜对了\n");
+//			break;
+//		}
+//	}
+//}
+//int main()
+//{
+//	int input = 0;
+//	srand((unsigned)time(NULL));
+//	do
+//	{
+//		menu();
+//		printf("请选择>:");
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case 1:
+//			game();
+//			break;
+//		case 0:
+//			break;
+//		default:
+//			printf("选择错误,请重新输入!\n");
+//			break;
+//		}
+//	} while (input);
+//	return 0;
+//}
+//
+
+
+
+//冒泡排序
+
+//void bubble_sort(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz - 1; i++)
+//	{
+//		int j = 0;
+//		int flag = 1;
+//		for (j = 0; j < sz - 1 - i; j++)
+//		{
+//			if (arr[j]> arr[j + 1])
+//			{
+//				int tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//				flag = 0;
+//			}
+//		}
+//		if (flag == 1)
+//			break;
+//	}
+//}
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	bubble_sort(arr, sz);
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//
+
+
+
+//实现函数init() 初始化数组为全0
+//实现print()  打印数组的每个元素
+//实现reverse()  函数完成数组元素的逆置。
+
+
+//void init(int arr[])
+//{
+//	
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = 0;
+//		printf("%d ", arr[i]);
+//	}
+//
+//}
+//
+//
+//void print(int arr[])
+//{
+//	
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = 0;
+//		printf("%d ", arr[i]);
+//	}
+//
+//}
+//
+//int main()
+//{
+//	int arr[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	init(arr[10]);
+//
+//}
+
+
+
+//交换两个变量，不创建临时变量
