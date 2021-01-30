bb5bfa1c02d3c3bf15fd5c6d1369339d6e84b914
hucaixin <77830983+hucaixin@users.noreply.github.com>
2021-01-30 16:39:34 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,143 +1,444 @@
-ï»¿#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-//çŒœæ•°å­—æ¸¸æˆ
+
+//ÇónµÄ½×³ËÓÃµİ¹éºÍÆÕÍ¨½â·¨
+
+int factorial(int n)
+{
+	int result = 1;
+	while (n > 1)
+	{
+		result *= n;
+		n -= 1;
+	}
+	return result;
+}
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	printf("%d ", factorial(n));
+	return 0;
+}
+
+
+
+//int factorial(int n)
+//{
+//	if (n <= 1)
+//		return 1;
+//	else
+//		return n*factorial(n - 1);
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	printf("%d ", factorial(a));
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i, n;
+//	double sum = 1;
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//		sum = sum*i;
+//	printf("%d!=%lf", n, sum);
+//	printf("\n");
+//	return 0;
+//}
+
+//µİ¹éºÍ·Çµİ¹é·Ö±ğÊµÏÖstrlen
+
+//#include<assert.h>
+//int my_strlen(const char* str)
+//{
+//	assert(str != NULL);
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	int len = my_strlen(arr);
+//	printf("%d\n", len);
+//	return 0;
+//}
+
+
 //#include<stdlib.h>
-//#include<time.h>
-//void menu()
+//#include<assert.h>
+//
+//int my_strlen(const char *str)
 //{
-//	printf("*****************************\n");
-//	printf("*********** 1.play **********\n");
-//	printf("*********** 0.exit **********\n");
-//	printf("*****************************\n");
+//	assert(str != NULL);
+//	if (*str == '\0')
+//	{
+//		return 0;
+//	}
+//	else
+//	{
+//		return my_strlen(++str) + 1;
+//	}
 //}
 //
-//
-//void game()
+//int main()
 //{
-//	int random_num = rand() % 100 + 1;
-//	int input = 0;
-//	while (1)
+//	int ret = 0;
+//	char arr[10] = "abcdef";
+//	ret = my_strlen(arr);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+//½«²ÎÊı×Ö·û´®ÖĞµÄ×Ö·û·´ÏòÅÅÁĞ£¬²»ÊÇÄæĞò´òÓ¡
+
+//#include<assert.h>
+//size_t my_strlen(const char* str)
+//{
+//	assert(str != NULL);
+//	int count = 0;
+//	while (*str != '\0')
 //	{
-//		printf("è¯·è¾“å…¥çŒœçš„æ•°å­—>:");
-//		scanf("%d", &input);
-//		if (input > random_num)
-//		{
-//			printf("çŒœå¤§äº†\n");
-//		}
-//		else if (input < random_num)
-//		{
-//			printf("çŒœå°äº†\n");
-//		}
-//		else
-//		{
-//			printf("æ­å–œä½ ï¼ŒçŒœå¯¹äº†\n");
-//			break;
-//		}
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//
+//void reverse_string(char* str)
+//{
+//	int len = my_strlen(str);
+//	char tmp = *str;
+//	*str = *(str + len - 1);
+//	*(str + len - 1) = '\0';
+//	if (my_strlen(str + 1) >= 2)
+//		reverse_string(str + 1);
+//	*(str + len - 1) = tmp;
+//}
+//
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	reverse_string(arr);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+//	
+
+//¼ÆËãÒ»¸öÊıµÄÃ¿Î»Ö®ºÍ£¨µİ¹éÊµÏÖ£©
+
+//int DigitSum(size_t n)
+//{
+//	if (n > 9)
+//	{
+//		return DigitSum(n / 10) + n % 10;
+//	}
+//	else
+//	{
+//		return n;
 //	}
 //}
 //int main()
 //{
-//	int input = 0;
-//	srand((unsigned)time(NULL));
-//	do
+//	size_t num = 0;
+//	scanf("%u", &num);
+//	size_t ret = DigitSum(num);
+//	printf("%u\n", ret);
+//	return 0;
+//}
+
+//µİ¹éÊµÏÖnµÄk´Î·½
+
+//double Pow(int n, int k)
+//{
+//	if (k == 0)
+//		return 1;
+//	else if (k > 0)
+//		return n*Pow(n, k - 1);
+//	else
+//		return 1.0 / (Pow(n, -k));
+//}
+//int main()
+//{
+//	int n = 0;
+//	int k = 0;
+//	scanf("%d %d", &n, &k);
+//	double ret = Pow(n, k);
+//	printf("%lf\n", ret);
+//	return 0;
+//}
+
+
+//¼ÆËãì³²¨ÄÇÆõÊı
+
+
+//int fib(int n)
+//{
+//	int result;
+//	int pre_result;
+//	int next_older_result;
+//	result = pre_result = 1;
+//	while (n > 2)
 //	{
-//		menu();
-//		printf("è¯·é€‰æ‹©>:");
-//		scanf("%d", &input);
-//		switch (input)
+//		n -= 1;
+//		next_older_result = pre_result;
+//		pre_result = result;
+//		result = pre_result + next_older_result;
+//	}
+//	return result;
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	fib(a);
+//	return 0;
+//}
+
+//´òÓ¡ÕûÊı¶ş½øÖÆµÄÆæÊıÎ»ºÍÅ¼ÊıÎ»
+
+//void print(int m)
+//{
+//	int i = 0;
+//	for (i = 30; i >= 0; i -= 2)
+//	{
+//		printf("%d ", (m >> i) & 1);
+//	}
+//	printf("\n");
+//	for (i = 31; i >= 1; i -= 2)
+//	{
+//		printf("%d ", (m >> i) & 1);
+//	}
+//	printf("\n");
+//}
+//int main()
+//{
+//	int m = 0;
+//	scanf("%d", &m);
+//	print(m);
+//	return 0;
+//}
+
+//Ê¹ÓÃÖ¸Õë´òÓ¡Êı×éÄÚÈİ
+
+//void print(int* arr,int n)
+//{
+//	int* p = arr;
+//	int i = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		printf("%d ", *(p + i));
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	print(arr,sz);
+//	return 0;
+//}
+
+//ÇóSn = a + aa + aaa + aaaa + aaaaaµÄÇ°5ÏîÖ®ºÍ£¬ÆäÖĞaÊÇÒ»¸öÊı×Ö£¬
+//ÀıÈç£º2 + 22 + 222 + 2222 + 22222
+
+//int main()
+//{
+//	int a = 0;
+//	int n = 0;
+//	scanf("%d %d", &a, &n);
+//	int sum = 0;
+//	int i = 0;
+//	int k = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		k = k * 10 + a;
+//		sum += k;
+//	}
+//	printf("%d\n", sum);
+//
+//	return 0;
+//}
+
+
+//ÌâÄ¿ÄÚÈİ£º
+//ºÈÆûË®£¬1Æ¿ÆûË®1Ôª£¬2¸ö¿ÕÆ¿¿ÉÒÔ»»Ò»Æ¿ÆûË®£¬¸ø20Ôª£¬¿ÉÒÔ¶àÉÙÆûË®£¨±à³ÌÊµÏÖ£©¡£
+
+//int main()
+//{
+//	int money = 0;
+//	scanf("%d", &money);//20
+//	int total = 0;
+//	int empty = 0;
+//	total += money;
+//	empty = money;
+//
+//	while (empty>=2)
+//	{
+//		total += empty / 2;
+//		empty = empty / 2 + empty%2;
+//	}
+//	printf("total = %d\n", total);
+//	return 0;
+//}
+
+
+
+//int Digit(int num)
+//{
+//	//digitÊÇ¼ÆËãµÄ¸ÃÊıµÄÎ»Êı
+//	int n = 1;
+//	while (num > 9)
+//	{
+//		num = num / 10;
+//		n++;
+//	}
+//	return n;
+//}
+////ÇónµÄk´Î·½
+//int Power(int n, int k)
+//{
+//	if (n == 0)
+//	{
+//		return 0;
+//	}
+//	int product = 1;
+//	for (int i = 0; i < k; i++)
+//	{
+//		product *= n;
+//	}
+//
+//	return  product;
+//}
+////Çó¸ÃÊıÊÇ·ñÎªË®ÏÉ»¨Êı
+//int Narcissus(int num)
+//{
+//	int n = Digit(num);
+//	int sum = 0;
+//	int number = num;
+//	for (int i = 0; i < n; i++)
+//	{
+//		sum += Power(number % 10, n);
+//		number = number / 10;
+//	}
+//	if (sum == num)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return 0;
+//	}
+//	//·µ»ØÖµËµÃ÷£ºÎª1ÔòÊÇË®ÏÉ»¨Êı£¬Îª0Ôò²»ÊÇË®ÏÉ»¨Êı
+//}
+//int main()
+//{
+//	for (int i = 0; i < 10000; i++)
+//	{
+//		if (Narcissus(i) == 1)
 //		{
-//		case 1:
-//			game();
-//			break;
-//		case 0:
-//			break;
-//		default:
-//			printf("é€‰æ‹©é”™è¯¯,è¯·é‡æ–°è¾“å…¥!\n");
-//			break;
+//			printf("%d\n", i);
 //		}
-//	} while (input);
+//	}
 //	return 0;
 //}
 //
 
 
 
-//å†’æ³¡æ’åº
-
-//void bubble_sort(int arr[], int sz)
+//void PrintLine(int blank_count, int start_count)
 //{
 //	int i = 0;
-//	for (i = 0; i < sz - 1; i++)
+//	for (; i < blank_count; i++)
 //	{
-//		int j = 0;
-//		int flag = 1;
-//		for (j = 0; j < sz - 1 - i; j++)
-//		{
-//			if (arr[j]> arr[j + 1])
-//			{
-//				int tmp = arr[j];
-//				arr[j] = arr[j + 1];
-//				arr[j + 1] = tmp;
-//				flag = 0;
-//			}
-//		}
-//		if (flag == 1)
-//			break;
+//		printf(" ");  //´òÓ¡¿Õ¸ñ
+//	}
+//	for (i = 0; i < start_count; i++)
+//	{
+//		printf("*");
+//	}
+//	printf("\n");
+//}
+//void  PrintLingxing(int n)
+//   {
+//	//´òÓ¡ÉÏ°ë²¿·Ö
+//	int i = 1;
+//	for (i = 1; i < n; i++){
+//		PrintLine(n - i, 2 * i - 1);  // ´òÓ¡Ã¿Ò»ĞĞµÄ¿Õ¸ñºÍÃ¿Ò»ĞĞµÄĞÇºÅ
+//	}
+//	//´òÓ¡ÖĞ¼ä²¿·Ö
+//	PrintLine(0, 2 * n - 1);
+//	//´òÓ¡ÏÂ°ë²¿·Ö
+//	for (i = n - 1; i>0; i--)
+//	{
+//		PrintLine(n - i, 2 * i - 1);
 //	}
 //}
 //int main()
 //{
-//	int i = 0;
-//	int arr[10] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
-//	int sz = sizeof(arr) / sizeof(arr[0]);
-//	bubble_sort(arr, sz);
-//	for (i = 0; i < sz; i++)
-//	{
-//		printf("%d ", arr[i]);
-//	}
+//	PrintLingxing(7);    //¶Ô°ëµÄĞĞÊı
 //	return 0;
-//
-
-
-
-//å®ç°å‡½æ•°init() åˆå§‹åŒ–æ•°ç»„ä¸ºå…¨0
-//å®ç°print()  æ‰“å°æ•°ç»„çš„æ¯ä¸ªå…ƒç´ 
-//å®ç°reverse()  å‡½æ•°å®Œæˆæ•°ç»„å…ƒç´ çš„é€†ç½®ã€‚
-
-
-//void init(int arr[])
-//{
-//	
-//	int i = 0;
-//	for (i = 0; i < 10; i++)
-//	{
-//		arr[i] = 0;
-//		printf("%d ", arr[i]);
-//	}
-//
 //}
 //
-//
-//void print(int arr[])
+
+
+//¼ÆËãì³²¨ÄÇÆõÊı
+
+//int fib(int n)
 //{
-//	
-//	int i = 0;
-//	for (i = 0; i < 10; i++)
+//	if (n <= 2)
+//		return 1;
+//	else
+//		return fib(n - 1) + fib(n - 2);
+//}
+//int main()
+//{
+//	int n = 0;
+//	printf("ÇëÊäÈëÒ»¸öµÚn¸öÊı£¬µÚn¸öÊıµÄ·Æ²¨ÄÇÇĞÊıÎª£º\n");
+//	scanf("%d", &n);
+//	printf("%d\n", fib(n));
+//	system("pause");
+//	return 0;
+//}
+
+//int fib(int n)
+//{
+//	int a1 = 1;
+//	int a2 = 1;
+//	int a3 = 0;
+//	if (n <= 2)
+//		return 1;
+//	else
 //	{
-//		arr[i] = 0;
-//		printf("%d ", arr[i]);
+//		while (n > 2)
+//		{
+//			a3 = a1 + a2;
+//			a1 = a2;
+//			a2 = a3;
+//			n--;
+//		}
 //	}
-//
+//	return a3;
 //}
 //
 //int main()
 //{
-//	int arr[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-//	int sz = sizeof(arr) / sizeof(arr[0]);
-//	init(arr[10]);
-//
+//	int n = 0;
+//	printf("ÇëÊäÈëÒ»¸öµÚn¸öÊı£¬µÚn¸öÊıµÄ·Æ²¨ÄÇÇĞÊıÎª£º\n");
+//	scanf("%d", &n);
+//	printf("%d", fib(n));
+//	system("pause");
+//	return 0;
 //}
-
-
-
-//äº¤æ¢ä¸¤ä¸ªå˜é‡ï¼Œä¸åˆ›å»ºä¸´æ—¶å˜é‡
+//
+//
